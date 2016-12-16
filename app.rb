0fda404af255d46f9e32675b8ff85bfe76122ce1
disne_y <bold.disney@gmail.com>
2016-12-16 19:58:02 +0300

    Parse orders input

--- app.rb
+++ app.rb
@@ -27,11 +27,38 @@ get '/cart' do
 end
 
 post '/cart' do
-	@pre_order = params[:orders].split(',')
+	orders_input = params[:orders]
+	@orders = parse_orders_input orders_input
 	@products = Product.all
 
-	erb :cart
+	erb " HEllo #{@orders.inspect}"
 end
+
+orders_line = "product_1=1,product_2=1,product_3=2"
+
+def parse_orders_input orders_input
+
+	s1 = orders_input.split(/,/)
+
+	arr = []
+        
+	s1.each do |x|
+		s2 = x.split(/=/)
+
+		s3 = s2[0].split(/_/)
+
+		id = s3[1]
+		cnt = s2[1]
+
+		arr2 = [id, cnt]	
+		arr.push arr2
+
+	end
+	
+	return arr
+
+end
+
 
 post '/order' do
 	return  erb :order

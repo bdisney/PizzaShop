15bcef4617bff45ed6cc15ad6c2bbda2fd98cf7f
disne_y <bold.disney@gmail.com>
2016-12-18 12:58:29 +0300

    Refactoring: rename control

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ get '/cart' do
 end
 
 post '/cart' do
-	@orders_input = params[:orders]
+	@orders_input = params[:orders_input]
 	@items = parse_orders_input @orders_input
 	@products = Product.all
 

efeebaaf46b4b10f90cd943cd1b98b2b046c2bf1
disne_y <bold.disney@gmail.com>
2016-12-18 13:21:29 +0300

    Add localStroage.clear() on cancel

--- public/app.js
+++ public/app.js
@@ -73,6 +73,15 @@ function cart_get_orders()
  	return orders;
 }
 
+function cancel_order()
+{
+	window.localStorage.clear();
+	
+	update_orders_input();
+	update_orders_button();
+	
+}
+
 
 
     
